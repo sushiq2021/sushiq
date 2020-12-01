f3afdd016c704c3b01722e5d77482e9032b234c0
ganett33 <65261313+ganett33@users.noreply.github.com>
2020-12-01 16:56:27 +1300

    Adding page Ver2.3
    java script fix

--- script.js
+++ script.js
@@ -8,26 +8,10 @@ function openNav() {
     }
   }
   
-  // Automatic Slideshow - change image every 3 seconds
-  var myIndex = 0;
-  carousel();
-  
-  function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}
-    x[myIndex-1].style.display = "block";
-    setTimeout(carousel, 3000);
-  }
-  
   //Get the button
   var mybutton = document.getElementById("myBtn");
   
-  // When the user scrolls down 20px from the top of the document, show the button
+  //When the user scrolls down 20px from the top of the document, show the button
   window.onscroll = function() {scrollFunction()};
   
   function scrollFunction() {
@@ -38,7 +22,7 @@ function openNav() {
     }
   }
   
-  // When the user clicks on the button, scroll to the top of the document
+  //When the user clicks on the button, scroll to the top of the document
   function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
